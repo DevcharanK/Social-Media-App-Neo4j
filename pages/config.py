5c180223c4e2c4a8dbefca5c643e617110175d14
Devcharan Krishna Naik <86884195+DevcharanK@users.noreply.github.com>
2025-04-02 13:37:40 -0400

    Create config.py

---
+++ pages/config.py
@@ -0,0 +1,7 @@
+# Create a storage account on Azure 
+
+STORAGE_ACCOUNT_NAME = "<storage account name>"
+STORAGE_ACCOUNT_KEY = "<Storage Account Key>"
+CONTAINER_NAME = "<Container name>"
+
+AZURE_STORAGE_CONNECTION_STRING = f"DefaultEndpointsProtocol=https;AccountName={STORAGE_ACCOUNT_NAME};AccountKey={STORAGE_ACCOUNT_KEY};EndpointSuffix=core.windows.net"
